9645bde978a72f3a9c325887611ba5c2471a1025
Magmamaster <magmamaster3045@gmail.com>
2026-07-09 00:23:51 +0100

    Update RunningState.cs

--- PocketPal/StateMachine/States/RunningState.cs
+++ PocketPal/StateMachine/States/RunningState.cs
@@ -10,30 +10,46 @@ public sealed class RunningState : IPetState
 
     private double _duration;
 
+
     public void Enter(PetContext context)
     {
         _duration = 1.0 + context.Random.NextDouble() * 2.5;
     }
 
-    public IPetState? Update(PetContext context, double deltaSeconds)
+
+    public IPetState? Update(
+        PetContext context,
+        double deltaSeconds)
     {
         // Running to a mouse/taskbar click location
         if (context.Movement.HasTarget)
         {
-            double beforeX = context.Movement.Position.X;
-
             context.Movement.MoveTowardsTarget(
                 SpeedPixelsPerSecond,
-                deltaSeconds
-            );
+                deltaSeconds);
 
-            // Target disappeared = reached destination
+
+            // Reached destination
             if (!context.Movement.HasTarget)
             {
                 context.ForceSit = true;
 
+
+                // Static Mode:
+                // return to permanent resting behaviour
+                if (context.StaticMode)
+                {
+                    return context.Random.Next(2) == 0
+                        ? new SittingState()
+                        : new SleepingState();
+                }
+
+
+                // Normal mode:
+                // sit for 10 seconds after being clicked to move
                 return new SittingState(true);
             }
+
 
             return null;
         }
@@ -42,8 +58,7 @@ public sealed class RunningState : IPetState
         // Normal random running
         context.Movement.MoveHorizontal(
             SpeedPixelsPerSecond,
-            deltaSeconds
-        );
+            deltaSeconds);
 
 
         if (context.TimeInState >= _duration)
